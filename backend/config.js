e178ed150c8363841586c5b883efe51bab50bcbc
ganeshpratao1306 <ganeshpratapvishwakarma05@gmail.com>
2026-03-31 09:00:57 +0530

    dockerfile

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,5 @@
-export const PORT = 5555;
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGO_URL;
